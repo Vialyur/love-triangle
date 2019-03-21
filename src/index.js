3118fc064d0b0c727667b4e542a61553543109c0
Vital Yurau <vialyur@list.ru>
2019-03-21 21:08:09 +0300

    feat: calculatе of love triangles

--- src/index.js
+++ src/index.js
@@ -3,5 +3,22 @@
  * @returns number of love triangles
  */
 module.exports = function getLoveTrianglesCount(preferences = []) {
-  // your implementation
+  // определяем общее количество Spichonees
+  let numberOfSpichonees = preferences.length;
+  // объявляем переменную для количества Spichonnes, находящихся в любовных треугольниках
+  let SpichoneesInTriangles = 0;
+  // перебираем массив в поисках количества участников треугольников
+  for (let i = 0; i<numberOfSpichonees; i += 1) {
+    let Spichonee1 = preferences[i] - 1;
+    if (Spichonee1 == i) continue;
+    let Spichonee2 = preferences[Spichonee1] - 1;
+    if (Spichonee2 == Spichonee1) continue;
+    let Spichonee3 = preferences[Spichonee2] - 1;
+    if (Spichonee3 == Spichonee2) continue;
+    if (Spichonee3 == i) {
+      SpichoneesInTriangles += 1;
+    }
+    // находим количество треугольников
+  }
+  return SpichoneesInTriangles / 3; 
 };
